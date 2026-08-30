d673610d4254f0f8d84b3894679a8e13e5271e6e
Leo Tavares <leotav.sec@gmail.com>
2026-08-30 13:48:27 +1000

    Reword the Check-a-message example prompt to be less "not sure" framed
    "Not sure what to try?" leans on the reader's uncertainty, which reads
    a little condescending for an audience that doesn't want to feel
    talked down to. "Want to see how it works?" invites the same action
    without implying the reader is confused.

--- sfincoassist/sw.js
+++ sfincoassist/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "sfincoassist-v20260830d";
+const CACHE_NAME = "sfincoassist-v20260830e";
 const CORE_ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=20260830d",
-  "./app.js?v=20260830d",
+  "./style.css?v=20260830e",
+  "./app.js?v=20260830e",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
